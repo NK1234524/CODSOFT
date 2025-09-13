8e4774c3b694910c23f7e20e33a6a1f7fa1e6c56
Nishant kumar <nishant201kashyap@gmail.com>
2025-09-13 20:36:06 +0530

    update

--- Calculator.py
+++ Calculator.py
@@ -10,39 +10,44 @@ while True:
 
   if choice=="1":
     print("Welcome to addition")
-    while True:
+    while True:      
       num1 = int(input("Enter the number"))
       num2 = int(input("Enter the number"))
       print(num1+num2)
-      break
+      more = input("Do you want to add more numbers? (yes/no): ")
+      if more.lower() != "yes":
+       break
   elif choice=="2":
     print("Welcome to subtraction")
     while True:
-      num1 = int(input("Enter the number"))
-      num2 = int(input("Enter the number"))
-      print(num1-num2)
-      if num1<num2:
-        print("Negative integer: ",num1-num2)
-      break
+      num1 = int(input("Enter the number 1 : "))
+      num2 = int(input("Enter the number 2 : "))
+      sub = num1-num2
+      if sub<0 :
+       print("negative integer : ",num1-num2)
+      else:
+        print("positive integer : ",sub)
+      more = input("Do you want to subtract more numbers? (yes/no): ")
+      if more.lower() != "yes":
+        break
   elif choice=="3":
     print("Welcome to multiplication")
     while True:
       num1 = int(input("Enter the number"))
       num2 = int(input("Enter the number"))
       print(num1*num2)
-      break
+      more = input("Do you want to multiply more numbers? (yes/no): ")
+      if more.lower() != "yes":
+       break
   elif choice=="4":
     print("Welcome to division")
     while True:
       num1 = int(input("Enter the number"))
       num2 = int(input("Enter the number"))
       print(num1/num2)
+      more = input("Do you want to divide more numbers? (yes/no): ")
+      if more.lower() != "yes":
+        break
       if num2==0:
         print("Infinity")
       break
-  elif choice=="5" :
-    print("Exit")
-    break
-  else:
-    print("Invalid choice")
-      
